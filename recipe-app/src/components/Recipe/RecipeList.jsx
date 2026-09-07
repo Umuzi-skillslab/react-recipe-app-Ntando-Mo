bf9696489af30db98915f731e6fdd597647b476b
Ntando Molose <ntandomolose@gmail.com>
2026-09-07 23:08:38 +0200

    Improved recipe cards UI

--- recipe-app/src/components/Recipe/RecipeList.jsx
+++ recipe-app/src/components/Recipe/RecipeList.jsx
@@ -1,28 +1,23 @@
-import { useContext } from 'react';
-import PropTypes from 'prop-types';
-import RecipeCard from './RecipeCard';
-import { FavoritesContext } from '../../context/FavoritesContext';
-
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import RecipeCard from "./RecipeCard";
+import { FavoritesContext } from "../../context/FavoritesContext";
+import styles from "./RecipeList.module.css";
 const RecipeList = ({ recipes }) => {
   const { favorites, toggleFavorite } = useContext(FavoritesContext);
-
-  // Changed 'auto-fit' to 'auto-fill' below to lock the card sizes when items are removed
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '30px' }}>
+    <div className={styles.grid}>
+      {" "}
       {recipes.map((recipe) => (
-        <RecipeCard 
-          key={recipe.id} 
-          recipe={recipe} 
-          onFavorite={() => toggleFavorite(recipe)} 
+        <RecipeCard
+          key={recipe.id}
+          recipe={recipe}
+          onFavorite={() => toggleFavorite(recipe)}
           isSaved={favorites.some((fav) => fav.id === recipe.id)}
         />
-      ))}
+      ))}{" "}
     </div>
   );
 };
-
-RecipeList.propTypes = {
-  recipes: PropTypes.array.isRequired,
-};
-
+RecipeList.propTypes = { recipes: PropTypes.array.isRequired };
 export default RecipeList;
